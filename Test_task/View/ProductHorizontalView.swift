bb2a48f493a629db9a677583906120237dc6bbf5
Bidzhou <89260226211@mail.ru>
2024-08-10 13:22:28 +0300

    started second task

---
+++ Test_task/View/ProductHorizontalView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductHorizontalView.swift
+//  Test_task
+//
+//  Created by Frederico del' Bidzho on 10.08.2024.
+//
+
+import SwiftUI
+
+struct ProductHorizontalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductHorizontalView()
+}
