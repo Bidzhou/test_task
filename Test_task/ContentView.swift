75aca9ef39591ca9601041faab665c44cb83a698
Bidzhou <89260226211@mail.ru>
2024-08-10 03:28:34 +0300

    Initial Commit

---
+++ Test_task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_task
+//
+//  Created by Frederico del' Bidzho on 10.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_task/Test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_taskApp.swift
+//  Test_task
+//
+//  Created by Frederico del' Bidzho on 10.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
