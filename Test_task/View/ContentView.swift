b448a7ffd534acb6dfdd5c06354d9b73f1d46404
Bidzhou <89260226211@mail.ru>
2024-08-10 13:23:03 +0300

    started second task

--- Test_task/View/ContentView.swift
+++ Test_task/View/ContentView.swift
@@ -13,15 +13,23 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Image(uiImage: UIImage(named: "4cubes")!)
-                    .frame(width: 16, height: 16)
-                    .padding(24)
-                    .background(Color("mainIcon"))
-                    .cornerRadius(16)
+                Button {
+                    print("adkpfm")
+                } label : {
+                    Image(uiImage: UIImage(named: "4cubes")!)
+                        .frame(width: 16, height: 16)
+                        .padding(24)
+                        .background(Color("mainIcon"))
+                        .cornerRadius(16)
+                }
+
                 Spacer()
             }.frame(width: screen.width*0.95, height: 44)
         }
         Spacer()
+            
+        Image(uiImage: UIImage(named: "divider")!)
+            .padding(.vertical, 2)
         ScrollView(.vertical, showsIndicators: false) {
             LazyVGrid(columns: layoutForProduct, content: {
                 ForEach(products, id: \.id) { product in
--- Test_task/View/ProductHorizontalView.swift
+++ Test_task/View/ProductHorizontalView.swift
@@ -9,7 +9,67 @@ import SwiftUI
 
 struct ProductHorizontalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0){
+            Image(uiImage: UIImage(named: "divider")!)
+            HStack{
+                ZStack(alignment:.topLeading){
+                    Image(uiImage: UIImage(named: "item1")!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 144, height: 144)
+                    Text("удар по ценам")
+                        .font(.system(size: 10))
+                        .padding(.horizontal,10)
+                        .padding(.vertical, 3)
+                        .foregroundColor(.white)
+                        .background(Color("punchPrices"))
+                        .clipShape(.rect(topLeadingRadius: 6, bottomTrailingRadius: 6, topTrailingRadius: 6))
+                    VStack {
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            Text("25%")
+                                .font(.system(size: 16))
+                                .bold()
+                                .foregroundColor(Color("saleRed"))                   
+                        }
+                        
+                    }
+                }.frame(width: 144, height: 144)
+                .padding(.leading, 16)
+                //Spacer()
+                VStack{
+                    HStack(spacing: 4){
+                        Image(uiImage: UIImage(named: "Star")!)
+                            .resizable()
+                            .frame(width: 12, height: 12)
+                        Text("4.4")
+                            .font(.system(size: 12))
+                        Image(uiImage: UIImage(named: "dividerVer")!)
+                            .resizable()
+                        
+                            .frame(width: 1, height: 16)
+                        Text("19 отзывов")
+                            .font(.system(size: 12))
+                            .foregroundStyle(Color("country"))
+                    }.frame(height: 20)
+                    Text("coca-cola zero 500ml.")
+                        .font(.system(size: 12))
+                        .foregroundStyle(Color("grayName"))
+                        .frame(height: 14)
+                    Text("france")
+                        .font(.system(size: 12))
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(Color("country"))
+                        .frame(height: 14)
+                    
+                    
+                }
+
+                Spacer()
+            }.frame(width: screen.width, height: 176)
+        }
+
     }
 }
 
